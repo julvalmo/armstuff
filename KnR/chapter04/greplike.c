6cb0610ec7d2d45fdbeeda500bfa08badce0396b
Julioth <juliothvalmo@outlook.com>
2024-08-27 18:03:57 -0500

    exercise misc

---
+++ KnR/chapter04/greplike.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+
+main() /* find all lines matching a pattern */
+
+{
+
+  char line[MAXLINE];
+
+
+  while (get_line(line, MAXLINE) > 0)
+
+    if (index(line, "the") >= 0)
+
+      printf("%s", line);
+
+}
+
+
+get_line(s, lim) /* get line into s, return length */
+
+char s[];
+
+int lim;
+
+{
+
+    int c, i;
+
+
+    for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+
+        s[i] = c;
+
+    if (c == '\n') {
+
+        s[i] = c;
+
+        ++i;
+
+    }
+
+    s[i] = '\0';
+
+    return(i);
+
+}
+
+
+
+index(s, t) /* return index of t in s, -1 if none */
+
+char s[], t[] ;
+
+{
+
+  int i, j, k;
+
+
+  for (i = 0; s[i] != '\0'; i++) {
+
+    for (j=i, k=0; t[k]!='\0' && s[j]==t[k]; j++, k++)
+
+      ;
+
+    if (t[k] == '\0')
+
+      return(i);
+
+  }
+
+  return (-1) ;
+
+}
---
+++ toralize/toralize.c
@@ -0,0 +1,16 @@
+#include "toralize.h"
+
+int main(int argc, char *argv[])
+{
+    char *host; // hostname we want to connect to
+    int port;
+
+
+    int (argc < 3)
+    {
+        fprintf(stderr, "Usage: %s <host> <port>\n",
+                argv[0]);
+        return -1;
+    }
+    return 0;
+}
